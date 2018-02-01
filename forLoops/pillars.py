2e0705540495bac4ecde6c2761954eb4e9060083
Karamqaoud <Kqaoud599@lsr7.net>
2018-02-01 11:09:30 -0600

    push new

--- forLoops/pillars.py
+++ forLoops/pillars.py
@@ -1,10 +1,11 @@
 from mcpi.minecraft import Minecraft
 mc = Minecraft.create()
 
+height =10
 def setPillar(x, y, z, height):
     stairBlock = 156
     block = 155
-
+    
     mc.setBlocks(x - 1, y + height, z - 1, x + 1, y + height, z + 1, block, 1)
     mc.setBlock(x - 1, y + height - 1, z, stairBlock, 12)
     mc.setBlock(x + 1, y + height - 1, z, stairBlock, 13)
@@ -23,8 +24,10 @@ def setPillar(x, y, z, height):
 pos = mc.player.getTilePos()
 x, y, z = pos.x + 2, pos.y, pos.z
 
-for item in range(5):
+for item in range(0, 10):
     setPillar(x, y, z, height)
+    x += 6
+   
     
 
 
